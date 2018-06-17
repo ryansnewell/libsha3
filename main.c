76aa8c9089a07467d33ebf2b6a579ec4cf6674f7
Ryan Newell <ryansnewell@gmail.com>
2018-06-17 16:54:33 -0500

    Fixes #1, sizes/lengths are now in bytes and the necessary unit is used where needed.

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ int padSize;
 uint8_t* PAD;
 FILE* m; //input meassge file
 
-unsigned long long int round_constant[24] = { 0x0000000000000001,
+uint64_t round_constant[24] = { 0x0000000000000001,
 											  0x0000000000008082,
 											  0x800000000000808A,
 											  0x8000000080008000,
@@ -95,63 +95,53 @@ void setupMessageAndPad(uint8_t* message){
 	}
 }
 
-// state is a 3d array of dimensions 5x5xW
-// postition [i][j][k] is accessed by [x*5*W + j*W + k]
-void theta(uint8_t*  state){
-	uint8_t* C = malloc(5*W);
+// Rotate input, word, by rot_amount bits.
+uint64_t rot64(uint64_t word, int rot_amount){
+	uint8_t temp;
+	rot_amount = rot_amount % 64;
+	return (word >> rot_amount) | (word << (64-rot_amount));
+}
+
+void theta(uint64_t*  state){
+	uint64_t* C = malloc(5);
 	for(int sheet = 0; sheet < 5; sheet++){
-		for(int z = 0; z < W; z++){
-			int x = sheet*5*W;
-			C[x*5 + z] = state[x + z] ^ state[x + z + W] ^ state[x + z + W*2] ^ state[x + z + W*3] ^ state[x + z + W*4]; 
-		}
+		int x = sheet*5;
+		C[sheet] = state[sheet] ^ state[sheet + 1] ^ state[sheet + 2] ^ state[sheet + 3] ^ state[sheet + 4]; 
 	}
 
-	uint8_t last = 0;
-	uint8_t next = 0;
-	uint8_t D[5][W];
-	memset(D, 0, 5*W);
+	uint64_t* D = malloc(5);
 	for(int x = 0; x < 5; x++){
-		for(int z = 0; z < W; z++){
-			D[(x+1)%5][z] ^= C[5*x+z];
-			next = C[5*x+z] & 1;
-			C[5*x+z] = (C[5*x+z] >> 1) | (last << 8);
-			last = next;
-			D[(x+4)%5][z] ^= C[5*x+z];
-		}
-	}
+		uint64_t c_rot = rot64(C[(x+1)%5], 1);
+		D[x] = C[(x-1) % 5] ^ c_rot;
 
-	for(int x = 0; x < 5; x++){
 		for(int y = 0; y < 5; y++){
-			for(int z = 0; z < W; z++){
-				state[x*W*5 + y*W + z] ^= D[x][z];
-			}
+				state[x*5 + y] ^= D[x];
 		}
 	}
 }
 
-void pi(uint8_t* state){
-	uint8_t* temp_B = malloc(25*W);
-	for(int i = 0; i < 25*W; i++){
+void pi(uint64_t* state){
+	uint64_t* temp_B = malloc(25);
+	for(int i = 0; i < 25; i++){
 		temp_B[i] = state[i];
 	}
 	
-	for(int z = 0; z < W; z++){
-		for(int x = 0; x < 5; x++){
-			for(int y = 0; y < 5; y++){
-				int new_x = y % 5;
-				int new_y = (2*x + 3*y) % 5;
-				
-				state[new_x*W*5 + new_y*W + z] = temp_B[x*W*5 + y*W + z];
-			}
+
+	for(int x = 0; x < 5; x++){
+		for(int y = 0; y < 5; y++){
+			int new_x = y % 5;
+			int new_y = (2*x + 3*y) % 5;
+
+			state[new_x*5 + new_y] = temp_B[x*5 + y];
 		}
 	}
 
 	free(temp_B);
 }
 
-void rho(uint8_t* state){
-	uint8_t* temp_B = malloc(25*W);
-	for(int i = 0; i < 25*W; i++){
+void rho(uint64_t* state){
+	uint64_t* temp_B = malloc(25);
+	for(int i = 0; i < 25; i++){
 		temp_B[i] = state[i];
 	}
 	
@@ -159,12 +149,8 @@ void rho(uint8_t* state){
 	int y = 0;
 	for(int t = 0; t < 24; t++){
 		int offset = ((t+1) * (t+2)) / 2;
-		// state(x,y) = ROT(temp_B, offset)
+		state[x*5 + y] = rot64(temp_B[x*5 + y], offset);
 		
-		for(int z = 0; z < W; z++){
-			state[5*x*W + 5*y + z] = temp_B[5*x*W + 5*y + ((z + offset) % W)];
-		}
-
 		x = y;
 		y = 2*x + 3*y;
 	}
@@ -172,30 +158,23 @@ void rho(uint8_t* state){
 	free(temp_B);
 }
 
-void chi(uint8_t* state){
-	uint8_t* temp_B = malloc(25*W);
-	for(int i = 0; i < 25*W; i++){
+void chi(uint64_t* state){
+	uint64_t* temp_B = malloc(25);
+	for(int i = 0; i < 25; i++){
 		temp_B[i] = state[i];
 	}
 
 	for(int x = 0; x < 5; x++){
 		for(int y = 0; y < 5; y++){
-			for(int z = 0; z < W; z++){
-				state[5*x*W + 5*y + z] = temp_B[5*x*W + 5*y + z] ^ ( ~(temp_B[5*W*((x+1)%5) + 5*y + z]) & temp_B[5*W*((x+2)%5) + 5*y + z] );
-			}
+				state[5*x + y] = temp_B[5*x + y] ^ ( ~(temp_B[5*((x+1)%5) + y]) & temp_B[5*((x+2)%5) + y]);
 		}
 	}
 
 	free(temp_B);
 }
 
-void iota(uint8_t* state){
-	printf("\tsizeof(uint8_t)=%lu\n", sizeof(uint8_t));
-	for(int z = W - 1; z >= 0; z--){
-		unsigned long long int rconst = (round_constant[NUM_ROUNDS - 1] >> ((W-z) * sizeof(uint8_t)));
-		printf("\tz: %i\tpre-Xor constant: %llu\n", z, rconst);
-		state[z] = state[z] ^ rconst;
-	}
+void iota(uint64_t* state){
+	state[0] = state[0] ^ round_constant[NUM_ROUNDS - 1];
 }
 
 void cleanup(){
@@ -207,7 +186,7 @@ void cleanup(){
 /*
  *Input:
  *	arg[1] = finename
- *	arg[2] = desired output length
+ *	arg[2] = desired output length (bits)
 */
 int main(int argc, char** argv){
 	if(argc < 3){
@@ -218,17 +197,17 @@ int main(int argc, char** argv){
 		printf("arg[2] = %s\n", argv[2]);
 	}
 
-	B =  1600;
-	C =  256; //TODO change to non static
+	B =  200; //1600 bits = 200 bytes
+	C =  32;  //256  bits = 32 bytes //TODO change to non static
 	R = B - C;
-	W = B / 25;
+	W = B / 25; //8 bytes = uint64_t or 8 bytes
 	NUM_ROUNDS = 24; //TODO change to non static???
-	OUTPUT_LENGTH = atoi(argv[2]);
+	OUTPUT_LENGTH = atoi(argv[2]) / 8;
 
 	loadFile(argv[1]);
 	setPAD();
 	
-	uint8_t* state   = malloc(25*W);
+	uint64_t* state  = malloc(25); //Make type uint64_t = malloc(25)
 	uint8_t* message = malloc(mSize + padSize);
 
 	if(!message){
