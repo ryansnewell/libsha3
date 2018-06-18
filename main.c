5d0282c943e51e8bd91b424ea4cd27eaffebfaed
Ryan Newell <ryansnewell@gmail.com>
2018-06-17 19:22:21 -0500

    Implements absorption, fixes #2 by adding a keccak function that loops through each round

--- main.c
+++ main.c
@@ -15,29 +15,29 @@ uint8_t* PAD;
 FILE* m; //input meassge file
 
 uint64_t round_constant[24] = { 0x0000000000000001,
-											  0x0000000000008082,
-											  0x800000000000808A,
-											  0x8000000080008000,
-											  0x000000000000808B,
-											  0x0000000080000001,
-											  0x8000000080008081,
-											  0x8000000000008009,
-											  0x000000000000008A,
-											  0x0000000000000088,
-											  0x0000000080008009,
-											  0x000000008000000A,
-											  0x000000008000808B,
-											  0x800000000000008B,
-											  0x8000000000008089,
-											  0x8000000000008003,
-											  0x8000000000008002,
-											  0x8000000000000080,
-											  0x000000000000800A,
-											  0x800000008000000A,
-											  0x8000000080008081,
-											  0x8000000000008080,
-											  0x0000000080000001,
-											  0x8000000080008008};
+							    0x0000000000008082,
+								0x800000000000808A,
+								0x8000000080008000,
+								0x000000000000808B,
+								0x0000000080000001,
+								0x8000000080008081,
+								0x8000000000008009,
+								0x000000000000008A,
+								0x0000000000000088,
+								0x0000000080008009,
+								0x000000008000000A,
+								0x000000008000808B,
+								0x800000000000008B,
+								0x8000000000008089,
+								0x8000000000008003,
+								0x8000000000008002,
+								0x8000000000000080,
+								0x000000000000800A,
+								0x800000008000000A,
+								0x8000000080008081,
+								0x8000000000008080,
+								0x0000000080000001,
+								0x8000000080008008};
 
 
 
@@ -93,23 +93,57 @@ void setupMessageAndPad(uint8_t* message){
 			message[i] = PAD[i-mSize];
 		}
 	}
+	free(PAD);
+	fclose(m);
+}
+
+//Get the n-th block from message(of size R), concat 0's(of size capacity C),
+//	and XOR it with the current state.
+//
+//AKA absorb the next message block and capacity into the state
+void absorb(uint64_t* state, uint8_t* message, int n){
+	uint64_t tempLane = 0x0000000000000000;
+	
+	//Loop through x,y = 0,1,2,3,4, the indices of the state array
+	//For every 64 bytes of the message block, XOR it with the state
+	//
+	//Since the message block is indexed by byte, looping through 64 bytes at a time,
+	//	store it in tempLane, then XOR it into the state
+	int gatheredBytes = 0;
+	for(int x = 0; x < 5; x++){
+		for(int y = 0; y < 5; y++){
+			for(int z = 0; z < W; z++){
+				if(gatheredBytes < R){
+					tempLane = tempLane << 8;
+					tempLane = tempLane ^ message[n*R + gatheredBytes];
+					gatheredBytes++;
+				}else{
+					tempLane = tempLane << 8;
+					tempLane = tempLane ^ 0b0000000;
+					gatheredBytes++;
+				}
+			}
+			
+			state[x*5 + y] = state[x*5 + y] ^ tempLane;
+			tempLane = 0x0000000000000000;
+		}
+	}
 }
 
 // Rotate input, word, by rot_amount bits.
 uint64_t rot64(uint64_t word, int rot_amount){
-	uint8_t temp;
 	rot_amount = rot_amount % 64;
 	return (word >> rot_amount) | (word << (64-rot_amount));
 }
 
 void theta(uint64_t*  state){
-	uint64_t* C = malloc(5);
+	uint64_t* C = malloc(5*sizeof(uint64_t));
 	for(int sheet = 0; sheet < 5; sheet++){
 		int x = sheet*5;
-		C[sheet] = state[sheet] ^ state[sheet + 1] ^ state[sheet + 2] ^ state[sheet + 3] ^ state[sheet + 4]; 
+		C[sheet] = state[x] ^ state[x + 1] ^ state[x + 2] ^ state[x + 3] ^ state[x + 4]; 
 	}
-
-	uint64_t* D = malloc(5);
+	
+	uint64_t* D = malloc(5*sizeof(uint64_t));
 	for(int x = 0; x < 5; x++){
 		uint64_t c_rot = rot64(C[(x+1)%5], 1);
 		D[x] = C[(x-1) % 5] ^ c_rot;
@@ -121,7 +155,7 @@ void theta(uint64_t*  state){
 }
 
 void pi(uint64_t* state){
-	uint64_t* temp_B = malloc(25);
+	uint64_t* temp_B = malloc(25*sizeof(uint64_t));
 	for(int i = 0; i < 25; i++){
 		temp_B[i] = state[i];
 	}
@@ -140,7 +174,7 @@ void pi(uint64_t* state){
 }
 
 void rho(uint64_t* state){
-	uint64_t* temp_B = malloc(25);
+	uint64_t* temp_B = malloc(25*sizeof(uint64_t));
 	for(int i = 0; i < 25; i++){
 		temp_B[i] = state[i];
 	}
@@ -159,7 +193,7 @@ void rho(uint64_t* state){
 }
 
 void chi(uint64_t* state){
-	uint64_t* temp_B = malloc(25);
+	uint64_t* temp_B = malloc(25*sizeof(uint64_t));
 	for(int i = 0; i < 25; i++){
 		temp_B[i] = state[i];
 	}
@@ -173,15 +207,27 @@ void chi(uint64_t* state){
 	free(temp_B);
 }
 
-void iota(uint64_t* state){
-	state[0] = state[0] ^ round_constant[NUM_ROUNDS - 1];
+void iota(uint64_t* state, int round){
+	state[0] = state[0] ^ round_constant[round - 1];
 }
 
-void cleanup(){
-	free(PAD);
-	
-	fclose(m);
+void keccak_f(uint64_t* state){	
+	for(int n = 0; n < NUM_ROUNDS; n++){
+		//printf("Starting theta step...\n");
+		theta(state);
+		//printf("Theta step complete. Starting rho step...\n");
+		rho(state);
+		//printf("Rho step complete. Starting pi step...\n");
+		pi(state);
+		//printf("Pi step complete. Starting chi step...\n");
+		chi(state);
+		//printf("Chi step complete. Starting iota step...\n");
+		iota(state, n);
+		//printf("Iota step complete. Starting clean up...\n");
+	}
+	//printf("Completed 24 rounds\n");
 }
+
 
 /*
  *Input:
@@ -207,8 +253,10 @@ int main(int argc, char** argv){
 	loadFile(argv[1]);
 	setPAD();
 	
-	uint64_t* state  = malloc(25); //Make type uint64_t = malloc(25)
-	uint8_t* message = malloc(mSize + padSize);
+	int messageSize = mSize + padSize;
+	uint8_t* message = malloc(messageSize);
+	uint64_t* state  = malloc(25*sizeof(uint64_t));
+	memset(state, 0, 25);
 
 	if(!message){
 		printf("Error allocating memory\n");
@@ -216,24 +264,12 @@ int main(int argc, char** argv){
 
 	setupMessageAndPad(message);
 	
-	printf("Setup complete. Starting theta step...\n");
-	theta(state);
-
-	printf("Theta step complete. Starting rho step...\n");
-	rho(state);
+	for(int p = 0; p < messageSize / R; p++){
+		//printf("Absorbing message block: %i\n", p);
+		absorb(state, message, p);
+		keccak_f(state);
+	}
 	
-	printf("Rho step complete. Starting pi step...\n");
-	pi(state);
-
-	printf("Pi step complete. Starting chi step...\n");
-	chi(state);
-
-	printf("Chi step complete. Starting iota step...\n");
-	iota(state);
-
-	printf("Iota step complete. Starting clean up...\n");
-	cleanup();
-
 	printf("Completed successfully. Exiting\n");
 	return 0;
 }
